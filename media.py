a275b64a9315d06182cf2d70c804f269bbe899c9
Luciano Baraúna <baraunaluciano@gmail.com>
2019-02-09 19:22:18 -0200

    create class media

--- media.py
+++ media.py
@@ -8,7 +8,6 @@ class Media():
         trailer_youtube_url: A string with the trailer link for youtube
     """
     def __init__(self, title, poster_image_url, trailer_youtube_url):
-            self.title = title
-            self.poster_image_url = poster_image_url
-            self.trailer_youtube_url = trailer_youtube_url
- 
+        self.title = title
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
